6a3958dd9f6e3c58360f9537a95923e16e0e354c
Thane Woidan <twoidan@nerdery.com>
2013-04-22 10:51:16 -0500

    Initial commit, adding files

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- META DATA -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <!--[if IE]>
+        <meta name="MSSmartTagsPreventParsing" content="true" />
+        <meta http-equiv="imagetoolbar" content="no" />
+        <meta http-equiv="cleartype" content="on" />
+        <![endif]-->
+
+        <!-- SEO -->
+        <title>Dialog</title>
+
+        <!-- MODERNIZR -->
+        <script type="text/javascript" src="assets/scripts/modernizr.js"></script>
+
+        <!-- STYLESHEETS -->
+        <link rel="stylesheet" media="screen, projection" href="assets/styles/screen.css" />
+        <!--[if IE 9]><link rel="stylesheet" type="text/css" media="screen, projection" href="assets/styles/ie9.css" /><![endif]-->
+        <!--[if lte IE 8]><link rel="stylesheet" type="text/css" media="screen, projection" href="assets/styles/ie8.css" /><![endif]-->
+
+        <!-- JQUERY -->
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script type="text/javascript">window.jQuery || document.write('<script type="text/javascript" src="assets/scripts/jquery.min.js"><\/script>')</script>
+
+        <!-- JAVASCRIPT -->
+        <script type="text/javascript" src="assets/scripts/global.js"></script>
+    </head>
+<body>
+
+</body>
+</html>
